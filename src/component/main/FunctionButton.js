0cba3befdc2f23d133397c90cefeaf410f053367
newive <738767136@qq.com>
2020-09-02 21:35:13 +0800

    '1.3'

--- src/component/main/FunctionButton.js
+++ src/component/main/FunctionButton.js
@@ -6,20 +6,20 @@ import './FunctionButton.scss';
 export default function FunctionButton(p) {
     return (
         <div className="function_panel">
-            <div className="function" onClick={p.commentHandler}>
-                <div className="img">
-                    <img src={comment} alt='留言'/>
-                </div>
-                <div className={'function_label'}>
-                    留言
-                </div>
-            </div>
             <div className="function" onClick={p.toEdit}>
                 <div className="img">
                     <img src={edit} alt="编辑"/>
                 </div>
                 <div className="function_label">
                     编辑
+                </div>
+            </div>
+            <div className="function" onClick={p.commentHandler}>
+                <div className="img">
+                    <img src={comment} alt='留言'/>
+                </div>
+                <div className={'function_label'}>
+                    留言
                 </div>
             </div>
         </div>
